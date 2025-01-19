2892b0326b8fb876d2837a087f64b01a0a78be50
Vladyslav Skakun <vskakun275l@gmail.com>
2025-01-20 00:00:00 +0100

    Добавил чтоб время менялось под настоящее

--- Front_End/main.js
+++ Front_End/main.js
@@ -1,12 +1,22 @@
 
+// Функция для вывода времени
+function updateTime() {
+    const now = new Date(); // Получаем текущее время
+    const hours = now.getHours().toString().padStart(2, '0'); // Часы (с ведущим нулем)
+    const minutes = now.getMinutes().toString().padStart(2, '0'); // Минуты (с ведущим нулем)
 
+    // Получаем смещение временной зоны в минутах и преобразуем его в часы
+    const timezoneOffset = now.getTimezoneOffset();
+    const sign = timezoneOffset > 0 ? '-' : '+'; // Определяем знак (минус для восточного времени, плюс для западного)
+    const hoursOffset = Math.abs(timezoneOffset) / 60; // Переводим смещение в часы
+    const offsetString = `(UTC${sign}${hoursOffset})`; // Форматируем смещение без ведущих нулей
 
+    const timeString = `${hours}:${minutes}`; // Формируем строку времени в локальном часовом поясе
 
-
-
-
-
-
+    // Обновляем содержимое элемента, добавляя время и временную зону
+    document.getElementById('TimeBoxItem').textContent = `${timeString} ${offsetString}`;
+}
+updateTime()
 
 
 /* Скорость фонового видео */
